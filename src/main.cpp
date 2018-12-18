190438db0696017e4eeae00331adf12fba660d4a
magras <magras@users.noreply.github.com>
2018-12-18 13:58:06 +0300

    added gtest and extended test suite

--- src/main.cpp
+++ src/main.cpp
@@ -75,25 +75,62 @@ std::optional<Point3d> triangle_line_segment_cross(Triangle const& tri, LineSegm
     return std::nullopt;
 }
 
-int main() {
+#include <gtest/gtest.h>
 
-    auto tri = Triangle{
-        Point3d{1,0,0},
-        Point3d{0,2,0},
-        Point3d{0,0,3}};
+TEST(TriangleLineSegmentCross, TrivialCase) {
+    auto x = triangle_line_segment_cross(
+        Triangle{
+            Point3d{1,0,0},
+            Point3d{0,1,0},
+            Point3d{0,0,1}},
+        LineSegment{
+            Point3d{0,0,0},
+            Point3d{1,1,1}}
+    );
+    ASSERT_TRUE(x);
+    ASSERT_DOUBLE_EQ(1./3, x.value()[0]);
+    ASSERT_DOUBLE_EQ(1./3, x.value()[1]);
+    ASSERT_DOUBLE_EQ(1./3, x.value()[2]);
+}
 
-    auto ln = LineSegment{
-        Point3d{0,0,0},
-        Point3d{1,1,1}};
+TEST(TriangleLineSegmentCross, NoIntersection) {
+    auto x = triangle_line_segment_cross(
+        Triangle{
+            Point3d{1,0,0},
+            Point3d{0,1,0},
+            Point3d{0,0,1}},
+        LineSegment{
+            Point3d{0,0,0},
+            Point3d{-1,-1,-1}}
+    );
+    ASSERT_FALSE(x);
+}
 
-    auto res = triangle_line_segment_cross(tri, ln);
+TEST(TriangleLineSegmentCross, FlatOrbitraryInput0) {
+    auto x = triangle_line_segment_cross(
+        Triangle{
+            Point3d{ 8,-3, 0},
+            Point3d{-7, 2, 0},
+            Point3d{-6,-1, 0}},
+        LineSegment{
+            Point3d{ 5, 6,-4},
+            Point3d{ 7, 9,-2}}
+    );
+    ASSERT_FALSE(x);
+}
 
-    if (res)
-        std::cout << res.value() << std::endl;
-    else
-        std::cout << "no intersection" << std::endl;
-
-    std::cin.ignore();
-
-    return 0;
+TEST(TriangleLineSegmentCross, FlatOrbitraryInput1) {
+    auto x = triangle_line_segment_cross(
+        Triangle{
+            Point3d{ 1, 3, 0},
+            Point3d{ 7,-3, 0},
+            Point3d{-1,-6, 0}},
+        LineSegment{
+            Point3d{ 0,-4, 1},
+            Point3d{-3,-7,-9}}
+    );
+    ASSERT_TRUE(x);
+    ASSERT_DOUBLE_EQ(-0.3, x.value()[0]);
+    ASSERT_DOUBLE_EQ(-4.3, x.value()[1]);
+    ASSERT_DOUBLE_EQ( 0.0, x.value()[2]);
 }
