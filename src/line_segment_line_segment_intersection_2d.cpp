fb601ef23c678ce5be93bc0281d3ebcb57258ebc
magras <magras@users.noreply.github.com>
2018-12-29 16:48:34 +0300

    add line segment - line segment on a plane intersection tests

--- src/line_segment_line_segment_intersection_2d.cpp
+++ src/line_segment_line_segment_intersection_2d.cpp
@@ -33,3 +33,71 @@ std::optional<Point2d> line_segment_line_segment_intersection(LineSegment2d cons
 
     return point_to_2d(*x);
 }
+
+#include <gtest/gtest.h>
+
+TEST(LineSegmentLineSegmentIntersection2d, TrivialCase) {
+    auto x = line_segment_line_segment_intersection(
+        LineSegment2d{
+            Point2d{0,0},
+            Point2d{1,1}},
+        LineSegment2d{
+            Point2d{0,1},
+            Point2d{1,0}}
+    );
+    ASSERT_TRUE(x);
+    EXPECT_DOUBLE_EQ(0.5, x.value()[0]);
+    EXPECT_DOUBLE_EQ(0.5, x.value()[1]);
+}
+
+TEST(LineSegmentLineSegmentIntersection2d, ParallelLines) {
+    auto x = line_segment_line_segment_intersection(
+        LineSegment2d{
+            Point2d{0,0},
+            Point2d{1,0}},
+        LineSegment2d{
+            Point2d{0,1},
+            Point2d{1,1}}
+    );
+    ASSERT_FALSE(x);
+}
+
+TEST(LineSegmentLineSegmentIntersection2d, CoincidentWithoutOverlap) {
+    auto x = line_segment_line_segment_intersection(
+        LineSegment2d{
+            Point2d{0,0},
+            Point2d{1,0}},
+        LineSegment2d{
+            Point2d{2,0},
+            Point2d{3,0}}
+    );
+    ASSERT_FALSE(x);
+}
+
+TEST(LineSegmentLineSegmentIntersection2d, Overlapping) {
+    auto x = line_segment_line_segment_intersection(
+        LineSegment2d{
+            Point2d{0,0},
+            Point2d{2,2}},
+        LineSegment2d{
+            Point2d{1,1},
+            Point2d{3,3}}
+    );
+    ASSERT_TRUE(x);
+    EXPECT_DOUBLE_EQ(1, x.value()[0]);
+    EXPECT_DOUBLE_EQ(1, x.value()[1]);
+}
+
+TEST(LineSegmentLineSegmentIntersection2d, PrimeNumbers) {
+    auto x = line_segment_line_segment_intersection(
+        LineSegment2d{
+            Point2d{0,0},
+            Point2d{173,179}},
+        LineSegment2d{
+            Point2d{83,17},
+            Point2d{19,89}}
+    );
+    ASSERT_TRUE(x);
+    EXPECT_DOUBLE_EQ(152759./2989, x.value()[0]); // 51,107059217129474740715958514553
+    EXPECT_DOUBLE_EQ(158057./2989, x.value()[1]); // 52,879558380729340916694546671127
+}
