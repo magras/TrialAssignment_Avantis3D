bcd80180dbe16016556fee71007554eafb11e549
magras <magras@users.noreply.github.com>
2018-12-29 18:11:52 +0300

    add comments and tests to triangle - line segment intersection on a plane

--- src/triangle_line_segment_intersection_2d.cpp
+++ src/triangle_line_segment_intersection_2d.cpp
@@ -25,13 +25,26 @@ std::optional<Point2d> line_segment_line_segment_intersection(LineSegment2d cons
 
 std::optional<Point2d> triangle_line_segment_intersection(Triangle2d const& tri, LineSegment2d const& ls) {
 
-    std::vector<Point2d> intersection;
+    // Basic idea of this algorithm is to find all points that intersect triangle
+    // and build "convex hull" for this set. It would allow to build line segment
+    // that represent all points of intersection.
+    
+    // If it's enough to return just any point, replace all
+    // `intersection.push_back()` calls with early returns.
 
+    // I like my algorithm, because it has no special cases, hence it's easy to
+    // implement.
+
+    std::vector<Point2d> intersection;
+    intersection.reserve(5);
+
+    // Add line segment ends to set, if they are inside of a triangle.
     for (Point2d const& p : ls) {
         if (is_point_inside_triangle(tri, p))
             intersection.push_back(p);
     }
 
+    // Find and add to set intersections between all triangle's edges and line segment.
     for (size_t i=0; i<tri.size(); ++i) {
         LineSegment2d edge{ tri[i], tri[i % tri.size()] };
         auto x = line_segment_line_segment_intersection(edge, ls);
@@ -39,6 +52,11 @@ std::optional<Point2d> triangle_line_segment_intersection(Triangle2d const& tri,
             intersection.push_back(*x);
     }
 
+    // Remove duplicates from the set.
+    // I'm assuming here, that duplicates would be exact match, because duplicates should
+    // appear in a set only if some of input data got duplicates in it. Therefore
+    // line segment - line segment intersection algorithm will return them without any
+    // modifications.
     std::sort(begin(intersection), end(intersection), [](Point2d const& lhs, Point2d const& rhs){
         return std::lexicographical_compare(begin(lhs), end(lhs), begin(rhs), end(rhs));
     });
@@ -46,11 +64,63 @@ std::optional<Point2d> triangle_line_segment_intersection(Triangle2d const& tri,
         std::unique(begin(intersection), end(intersection)),
         end(intersection));
 
+    // After removing duplicates there should be 0, 1, or 2 points in a set.
     assert(intersection.size() >= 0);
     assert(intersection.size() <= 2);
 
     if (intersection.empty())
         return std::nullopt;
 
+    // Instead of possibly building a line segment just return any point from the set.
     return intersection[0];
 }
+
+#include <gtest/gtest.h>
+
+TEST(TriangleLineSegmentIntersection2d, TouchingVetex) {
+    auto x = triangle_line_segment_intersection(
+        Triangle2d{
+            Point2d{0,0},
+            Point2d{0,1},
+            Point2d{1,0}},
+        LineSegment2d{
+            Point2d{-1,1},
+            Point2d{1,-1}}
+    );
+    ASSERT_TRUE(x);
+    EXPECT_DOUBLE_EQ(0, x.value()[0]);
+    EXPECT_DOUBLE_EQ(0, x.value()[1]);
+}
+
+TEST(TriangleLineSegmentIntersection2d, TouchingEdge) {
+    auto x = triangle_line_segment_intersection(
+        Triangle2d{
+            Point2d{0,0},
+            Point2d{0,1},
+            Point2d{1,0}},
+        LineSegment2d{
+            Point2d{0.5,0.5},
+            Point2d{1,2}}
+    );
+    ASSERT_TRUE(x);
+    EXPECT_DOUBLE_EQ(0.5, x.value()[0]);
+    EXPECT_DOUBLE_EQ(0.5, x.value()[1]);
+}
+
+TEST(TriangleLineSegmentIntersection2d, WholeLineSegmentInsideTriangle) {
+    auto x = triangle_line_segment_intersection(
+        Triangle2d{
+            Point2d{0,0},
+            Point2d{0,4},
+            Point2d{4,0}},
+        LineSegment2d{
+            Point2d{1,2},
+            Point2d{2,1}}
+    );
+    ASSERT_TRUE(x);
+    EXPECT_DOUBLE_EQ(1, x.value()[0]);
+    EXPECT_DOUBLE_EQ(2, x.value()[1]);
+}
+
+// DEBUG: It's hard to test this function properly. I think I'll change
+//        return type to allow line segment results.
