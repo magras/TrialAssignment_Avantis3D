1fc907a236d9d3d82d5f6f6647e65d5998a8308a
magras <magras@users.noreply.github.com>
2019-01-16 02:16:30 +0300

    improved tests for triangle — line segment intersection on a plane

--- src/triangle_line_segment_intersection_2d.cpp
+++ src/triangle_line_segment_intersection_2d.cpp
@@ -1,6 +1,6 @@
 #include <algorithm>
 #include <optional>
-#include <vector>
+#include "equal_to_zero.h"
 #include "geom_types.h"
 
 namespace {
@@ -9,7 +9,22 @@ namespace {
         return a[0] * b[1] - a[1] * b[0];
     }
 
-    bool is_point_inside_triangle(Triangle2d const& t, Point2d const& p) {
+    bool is_point_on_line_segment(Point2d const& p, LineSegment2d const& ls) {
+        
+        Point2d a = ls[1] - ls[0];
+        Point2d b = p - ls[0];
+        double n = sqrNorm(a);
+
+        // Check that point is on a line.
+        if (not_equal_to_zero(perp_dot(a, b), n))
+            return false;
+
+        // Check that point is inside line segment.
+        double x = dot(a, b);
+        return 0 <= x && x <= n;
+    }
+
+    bool is_point_inside_triangle(Point2d const& p, Triangle2d const& t) {
         std::array<double, 3> s {
             perp_dot(t[1] - t[0], p - t[0]),
             perp_dot(t[2] - t[1], p - t[1]),
@@ -27,7 +42,7 @@ std::optional<Point2d> triangle_line_segment_intersection(Triangle2d const& tri,
 
     // If any end of line segment is inside of the triangle return it.
     for (Point2d const& p : ls) {
-        if (is_point_inside_triangle(tri, p))
+        if (is_point_inside_triangle(p, tri))
             return p;
     }
 
@@ -45,50 +60,56 @@ std::optional<Point2d> triangle_line_segment_intersection(Triangle2d const& tri,
 
 #include <gtest/gtest.h>
 
-// CRAP: This tests are fragile. Probably should check that
-// returned point lies inside triangle and line segment.
+TEST(TriangleLineSegmentIntersection2d, NoIntersection) {
+    auto t = Triangle2d{
+        Point2d{0,0},
+        Point2d{0,1},
+        Point2d{1,0}};
+    auto ls = LineSegment2d{
+        Point2d{0,2},
+        Point2d{2,0}};
+    auto x = triangle_line_segment_intersection(t, ls);
+    ASSERT_FALSE(x);
+}
 
 TEST(TriangleLineSegmentIntersection2d, TouchingVetex) {
-    auto x = triangle_line_segment_intersection(
-        Triangle2d{
-            Point2d{0,0},
-            Point2d{0,1},
-            Point2d{1,0}},
-        LineSegment2d{
-            Point2d{-1,1},
-            Point2d{1,-1}}
-    );
+    auto t = Triangle2d{
+        Point2d{0,0},
+        Point2d{0,1},
+        Point2d{1,0}};
+    auto ls = LineSegment2d{
+        Point2d{-1,1},
+        Point2d{1,-1}};
+    auto x = triangle_line_segment_intersection(t, ls);
     ASSERT_TRUE(x);
-    EXPECT_DOUBLE_EQ(0, x.value()[0]);
-    EXPECT_DOUBLE_EQ(0, x.value()[1]);
+    ASSERT_TRUE(is_point_inside_triangle(x.value(), t));
+    ASSERT_TRUE(is_point_on_line_segment(x.value(), ls));
 }
 
 TEST(TriangleLineSegmentIntersection2d, TouchingEdge) {
-    auto x = triangle_line_segment_intersection(
-        Triangle2d{
-            Point2d{0,0},
-            Point2d{0,1},
-            Point2d{1,0}},
-        LineSegment2d{
-            Point2d{0.5,0.5},
-            Point2d{1,2}}
-    );
+    auto t = Triangle2d{
+        Point2d{0,0},
+        Point2d{0,1},
+        Point2d{1,0}};
+    auto ls = LineSegment2d{
+        Point2d{0.5,0.5},
+        Point2d{1,2}};
+    auto x = triangle_line_segment_intersection(t, ls);
     ASSERT_TRUE(x);
-    EXPECT_DOUBLE_EQ(0.5, x.value()[0]);
-    EXPECT_DOUBLE_EQ(0.5, x.value()[1]);
+    ASSERT_TRUE(is_point_inside_triangle(x.value(), t));
+    ASSERT_TRUE(is_point_on_line_segment(x.value(), ls));
 }
 
 TEST(TriangleLineSegmentIntersection2d, WholeLineSegmentInsideTriangle) {
-    auto x = triangle_line_segment_intersection(
-        Triangle2d{
-            Point2d{0,0},
-            Point2d{0,4},
-            Point2d{4,0}},
-        LineSegment2d{
-            Point2d{1,2},
-            Point2d{2,1}}
-    );
+    auto t = Triangle2d{
+        Point2d{0,0},
+        Point2d{0,4},
+        Point2d{4,0}};
+    auto ls = LineSegment2d{
+        Point2d{1,2},
+        Point2d{2,1}};
+    auto x = triangle_line_segment_intersection(t, ls);
     ASSERT_TRUE(x);
-    EXPECT_DOUBLE_EQ(1, x.value()[0]);
-    EXPECT_DOUBLE_EQ(2, x.value()[1]);
+    ASSERT_TRUE(is_point_inside_triangle(x.value(), t));
+    ASSERT_TRUE(is_point_on_line_segment(x.value(), ls));
 }
