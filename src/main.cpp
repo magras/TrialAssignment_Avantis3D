822ff66cd756901359dc9dc5bc728072f774a30d
magras <magras@users.noreply.github.com>
2019-01-15 22:44:34 +0300

    remove boost

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-// CRAP: Because of conflict between gtest and boost.test
-// I have to implement `main` myself. I'll deal with it later.
-
-#include <gtest/gtest.h>
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
