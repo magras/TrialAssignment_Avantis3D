d9c6fea74a85ecd66d3295a261ba9d7eda89d879
magras <magras@users.noreply.github.com>
2019-01-15 18:29:22 +0300

    perfect forwarding for fmap

--- src/fmap.cpp
+++ src/fmap.cpp
@@ -1,8 +1,8 @@
 #include "fmap.h"
-
+#include <memory>
 #include <gtest/gtest.h>
 
-TEST(FmapOptional, Integer) {
+TEST(Fmap, OptionalInteger) {
 
     std::optional<int> a {1};
     auto b = fmap(a, [](auto x){ return x + 2; });
@@ -15,7 +15,7 @@ TEST(FmapOptional, Integer) {
     ASSERT_EQ(3, *b);
 }
 
-TEST(FmapOptional, OptionalInteger) {
+TEST(Fmap, OptionalOptionalInteger) {
 
     std::optional<int> a {1};
     auto b = fmap(a, [](auto x){ return std::optional{x + 2}; });
@@ -27,4 +27,17 @@ TEST(FmapOptional, OptionalInteger) {
     ASSERT_TRUE(b);
     ASSERT_TRUE(*b);
     ASSERT_EQ(3, **b);
+}
+
+TEST(Fmap, OptionalMoveOnlyType) {
+
+    auto a = std::make_optional(std::make_unique<int>(2));
+    auto b = fmap(a, [](auto && p){ return std::make_unique<double>(*p * 0.25); });
+
+    static_assert(std::is_same_v<
+        std::optional<std::unique_ptr<double>>,
+        decltype(b)>
+    );
+    ASSERT_TRUE(b);
+    ASSERT_EQ(0.5, **b);
 }
--- src/fmap.h
+++ src/fmap.h
@@ -4,10 +4,22 @@
 #include <optional>
 #include <type_traits>
 
-template <typename T, typename F>
-std::optional<std::invoke_result_t<F, T const&>> fmap(std::optional<T> const& o, F f) {
-    if (o)
-        return f(*o);
+template <typename T> struct is_optional                   : std::false_type {};
+template <typename T> struct is_optional<std::optional<T>> : std::true_type  {};
+template <typename T> inline constexpr bool is_optional_v = is_optional<T>::value;
+
+// TODO: remove when P0550R2 will be implemented
+template <typename T> using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
+
+template <
+    typename O,
+    typename F,
+    typename = std::enable_if_t<is_optional_v<remove_cvref_t<O>>>
+>
+constexpr auto fmap(O && o, F f) noexcept -> std::optional<std::invoke_result_t<F, typename std::remove_reference_t<O>::value_type>>
+{
+    if (o.has_value())
+        return f(*std::forward<O>(o));
     return std::nullopt;
 }
 
